d85b9cb0f92b22631779b97165106307fc9fec66
Burkhard Kneiseler <burkhard@kneiseler.de>
2025-01-18 06:59:31 +0100

    FIX: ColorHelper.php - style

--- libs/ColorHelper.php
+++ libs/ColorHelper.php
@@ -292,7 +292,7 @@ trait ColorHelper
             ), 0);
 
             $value = max(0, min(100, $value));
-            $result = (int)(($value * ($oldMax - $oldMin) / 100) + $oldMin);
+            $result = (int) (($value * ($oldMax - $oldMin) / 100) + $oldMin);
         } else {
             $this->SendDebug(__FUNCTION__, sprintf(
                 'Converting device value %.2f (range %d-%d) to percent',
@@ -303,7 +303,7 @@ trait ColorHelper
 
             $value = max($oldMin, min($oldMax, $value));
             // Float zu Integer casten vor intdiv
-            $result = intdiv((int)($value - $oldMin) * 100, $oldMax - $oldMin);
+            $result = intdiv((int) ($value - $oldMin) * 100, $oldMax - $oldMin);
         }
 
         $this->SendDebug(__FUNCTION__, sprintf(
@@ -336,7 +336,7 @@ trait ColorHelper
             'max' => 254
         ];
 
-        $value = isset($config[$type]) ? (int)$config[$type] : $defaults[$type];
+        $value = isset($config[$type]) ? (int )$config[$type] : $defaults[$type];
         $this->SendDebug(__FUNCTION__, sprintf(
             'Brightness %s-Wert: %d (%s)',
             $type,
